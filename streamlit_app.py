132dfe08e839d4737441e0a21ec25367aa8e5fd1
fodaa711 <Mofoda2004@gmail.com>
2024-12-08 14:58:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,8 @@ import streamlit as st
 # Show the page title and description.
 st.set_page_config(page_title="Movies dataset", page_icon="🎬")
 st.title("🎬 Movies recommender dataset")
-option=st.selectbox('
-How would you be contacted?',
-('email','Home phone','mobile phone'))
+option = st.selectbox('How would you be contacted?', ('email', 'Home phone', 'mobile phone'))
+
 
 st.write(
     """
